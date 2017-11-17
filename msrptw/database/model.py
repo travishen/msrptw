1a933e59461f264581c54625496b5c86a81d7013
travishen <travishen.tw@gmail.com>
2017-11-17 16:36:17 +0800

    logging to sqlalchemy Log table

--- msrptw/database/model.py
+++ msrptw/database/model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-from sqlalchemy import Integer, Column, ForeignKey, Sequence, String, Unicode, Table
+from sqlalchemy import Integer, Column, ForeignKey, Sequence, String, Unicode, Date, DateTime, Boolean
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from json import JSONEncoder
 from . import _base
@@ -13,9 +14,6 @@ class Config(_base):
     name = Column(Unicode(5))
     parts = relationship('Part')
 
-    def parts_to_dict(self):
-        return {'(%s) : %s  ' % (i, part.name) for i, part in enumerate(self.parts)}
-
 
 class Part(_base):
     __tablename__ = 'part'
@@ -23,7 +21,7 @@ class Part(_base):
     config_id = Column(Integer, ForeignKey('config.id'))
     config = relationship('Config', back_populates='parts')
     products = relationship('Product')
-    name = Column(Unicode(5))
+    name = Column(Unicode(15))
 
 
 class Market(_base):
@@ -51,8 +49,38 @@ class Product(_base):
     origin = relationship('Origin', back_populates='products')
     name = Column(Unicode(30))
     pid = Column(String(20))
+    track = Column(Boolean)
+    prices = relationship('Price')
 
 
+class Price(_base):
+    __tablename__ = 'price'
+    product_id = Column(Integer, ForeignKey('product.id'), primary_key=True)
+    product = relationship('Product', back_populates='prices')
+    price = Column(Integer)
+    weight = Column(Integer)
+    date = Column(Date)
+
+
+class Log(_base):
+    __tablename__ = 'log'
+    id = Column(Integer, Sequence('log_id_seq'), primary_key=True, nullable=False)
+    logger = Column(String)
+    level = Column(String)
+    msg = Column(String)
+    datetime = Column(DateTime, default=func.now())
+
+    def __init__(self, logger=None, level=None, trace=None, msg=None):
+        self.logger = logger
+        self.level = level
+        self.trace = trace
+        self.msg = msg
+
+    def __unicode__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return "<Log: %s - %s>" % (self.created_at.strftime('%m/%d/%Y-%H:%M:%S'), self.msg[:50])
 
 
 
---
+++ msrptw/handler.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from .database.config import session_scope
+from .database.model import Log
+
+
+class SQLAlchemyHandler(logging.Handler):
+    def emit(self, record):
+        trace = None
+        log = Log(
+            logger=record.__dict__['name'],
+            level=record.__dict__['levelname'],
+            msg=record.__dict__['msg'], )
+        with session_scope() as session:
+            session.add(log)
